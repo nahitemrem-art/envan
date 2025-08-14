6cb9277eb9905d6d6b71468619d83bc2e2a41a57
nahitemrem-art <nahitemrem@gmail.com>
2025-08-14 14:11:53 +0300

    Update EnvanterContextFactory.cs

--- Data/EnvanterContextFactory.cs
+++ Data/EnvanterContextFactory.cs
@@ -4,12 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 
+
 public class EnvanterContextFactory : IDesignTimeDbContextFactory<EnvanterContext>
 {
     public EnvanterContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<EnvanterContext>();
-        optionsBuilder.UseNpgsql("Host=dpg-d2e7hoc9c44c73egrho0-a.oregon-postgres.render.com;Port=5432;Database=envanterdb;Username=envanterdb_user;Password=XqwzlDcZklWMQPsb01FBhnTqQllghjxN;SSL Mode=Require;Trust Server Certificate=true");
+        optionsBuilder.UseNpgsql("Host=dpg-d2e7hoc9c44c73egrho0-a.oregon-postgres.render.com;Port=5432;Database=envanterdb;Username=envanterdb_user;Password=XqwzlDcZklWMQPsb01FBhnTqQllghjxN;SSL Mode=Require");
         return new EnvanterContext(optionsBuilder.Options);
     }
 }
+
