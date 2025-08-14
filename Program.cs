d94bc2b6c4d12fb21c8c98ecc157e2dea0d07965
nahitemrem-art <nahitemrem@gmail.com>
2025-08-14 14:06:33 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -29,9 +29,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<EnvanterContext>();
-    db.Database.Migrate();
-}
+
 app.Run();
