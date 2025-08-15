a2d9503895b91d18135f824e2b7c9aef9fbcc2a8
nahitemrem-art <nahitemrem@gmail.com>
2025-08-15 15:11:47 +0300

    Update EnvanterContextFactory.cs

--- Data/EnvanterContextFactory.cs
+++ Data/EnvanterContextFactory.cs
@@ -2,15 +2,30 @@
 using EnvanterTakip.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
-
 
 public class EnvanterContextFactory : IDesignTimeDbContextFactory<EnvanterContext>
 {
     public EnvanterContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<EnvanterContext>();
-        optionsBuilder.UseNpgsql("postgresql://envanterdb_user:XqwzlDcZklWMQPsb01FBhnTqQllghjxN@dpg-d2e7hoc9c44c73egrho0-a/envanterdb");
+        
+        // PostgreSQL URL'ini parse et
+        var databaseUrl = "postgresql://envanterdb_user:XqwzlDcZklWMQPsb01FBhnTqQllghjxN@dpg-d2e7hoc9c44c73egrho0-a/envanterdb";
+        
+        string connectionString;
+        if (!string.IsNullOrEmpty(databaseUrl) && databaseUrl.StartsWith("postgresql://"))
+        {
+            // PostgreSQL URL'ini Npgsql formatına çevir
+            var uri = new Uri(databaseUrl);
+            var userInfo = uri.UserInfo.Split(':');
+            connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.Trim('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+        }
+        else
+        {
+            connectionString = databaseUrl;
+        }
+        
+        optionsBuilder.UseNpgsql(connectionString);
         return new EnvanterContext(optionsBuilder.Options);
     }
 }
